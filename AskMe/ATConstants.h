b97cfa21b7dfb60ab56c470c3e88398d2a33eec0
ryantan <squall3d@gmail.com>
2013-03-29 13:07:05 +0800

    questions controller and constants file

---
+++ AskMe/ATConstants.h
@@ -0,0 +1,12 @@
+//
+//  ATConstants.h
+//  AskMe
+//
+//  Created by Minitheory Design on 29/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+
+#pragma mark - NSNotification
+extern NSString *const ATAppDelegateApplicationDidReceiveRemoteNotification;
+
---
+++ AskMe/Controllers/ATAnswerController.h
@@ -0,0 +1,21 @@
+//
+//  ATAnswerController.h
+//  AskMe
+//
+//  Created by Minitheory Design on 29/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATAnswerController : NSObject {
+    
+    
+}
+
++ (ATAnswerController*)shared;
+
+
+
+
+@end
